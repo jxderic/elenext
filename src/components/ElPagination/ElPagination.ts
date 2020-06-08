b144864963438b944eb762ad8ecb95f0e7bb8935
cocoschen <cocoschen@wesure.cn>
2020-06-08 19:47:10 +0800

    feat: update ElPagination

--- src/components/ElPagination/ElPagination.ts
+++ src/components/ElPagination/ElPagination.ts
@@ -1,20 +1,29 @@
 import { defineComponent, h, mergeProps } from 'vue';
 
 interface ElPaginationProps {
+  pageSize: number;
   small: boolean;
   total: number;
   pageCount: number;
   pagerCount: number;
+  currentPage: number;
+  layout: string;
+  pageSizes: any[];
+  popperClass: string;
+  prevText: string;
+  nextText: string;
   background: boolean;
+  disabled: boolean;
+  hideOnSinglePage: boolean;
 }
 
 export default defineComponent({
   name: '',
   props: {
-    // pageSize: {
-    //   type: Number,
-    //   default: 10
-    // },
+    pageSize: {
+      type: Number,
+      default: 10
+    },
     small: {
       type: Boolean,
       default: false
@@ -33,35 +42,26 @@ export default defineComponent({
       },
       default: 7
     },
-
-    // currentPage: {
-    //   type: Number,
-    //   default: 1
-    // },
-
-    // layout: {
-    //   type: String,
-    //   default: 'prev, pager, next, jumper, ->, total'
-    // },
-
-    // pageSizes: {
-    //   type: Array,
-    //   default() {
-    //     return [10, 20, 30, 40, 50, 100];
-    //   }
-    // },
-
-    // popperClass: String,
-
-    // prevText: String,
-
-    // nextText: String,
-
-    background: Boolean
-
-    // disabled: Boolean,
-
-    // hideOnSinglePage: Boolean
+    currentPage: {
+      type: Number,
+      default: 1
+    },
+    layout: {
+      type: String,
+      default: 'prev, pager, next, jumper, ->, total'
+    },
+    pageSizes: {
+      type: Array,
+      default() {
+        return [10, 20, 30, 40, 50, 100];
+      }
+    },
+    popperClass: String,
+    prevText: String,
+    nextText: String,
+    background: Boolean,
+    disabled: Boolean,
+    hideOnSinglePage: Boolean
   },
   setup(props: ElPaginationProps, { attrs, slots, emit }) {
     return () =>
